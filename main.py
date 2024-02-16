5d267ffd332322839ffabefb5ff638e4a4ef5841
Nihal Kashinath <nihal.kashinath@gmail.com>
2024-02-16 11:44:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from langchain import LLMChain, PromptTemplate
 #from getpass import getpass
 
 #os.environ['TOGETHER_API_KEY'] = getpass()
-TOGETHER_API_KEY='7f78090fe88139a88680b5991a386120f08c7f38d98196c6448ee209df464142'
+TOGETHER_API_KEY=7f78090fe88139a88680b5991a386120f08c7f38d98196c6448ee209df464142
 
 llama_model = Together(
     model="meta-llama/Llama-2-70b-chat-hf",
